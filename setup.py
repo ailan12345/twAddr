bcbca5c2a31c5db5ea35ea226d936980f4c6b556
ailan12345 <karta2599434@gmail.com>
2019-08-28 20:56:34 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name = 'twAddr',
+    packages = ['zipcode'],
+    scripts = ['runner'],
+    version = '1.0',
+    description = 'Taiwan address related services',
+    author = 'Ailan',
+    author_email = 'karta2599434@gmail.com',
+    keywords = ['tw', 'addr'],
+    classifiers = [],
+    url = 'https://github.com/ailan12345/twAddr',
+
+    package_data = {'zipcode': ['*.xls']},
+
+)
