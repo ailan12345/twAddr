4a7492719fa4b942f4fbac15c083621d78335a00
ailan12345 <karta2599434@gmail.com>
2019-08-29 06:58:14 +0800

    com runner

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name = 'twAddr',
     packages = ['zipcode'],
-    scripts = ['runner'],
-    version = '1.0',
+    #scripts = ['runner'],
+    version = '1.0.1',
     description = 'Taiwan address related services',
     author = 'Ailan',
     author_email = 'karta2599434@gmail.com',
