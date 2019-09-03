747a940928e5f80f45fc4af62380d24e88d63c9c
ailan12345 <karta2599434@gmail.com>
2019-09-03 22:44:36 +0800

    1.0.6

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
-from distutils.core import setup
+import sys
+from twaddr import addrdata
+from setuptools.command.install import install
+
+
+class twAddr_install(install):
+    def run(self):
+        print('Building Addr ... ')
+        sys.stdout.flush()
+        twAddr.addrdata.createDB()
+        install.run(self)
+
+from setuptools import setup
 
 setup(
     name = 'twAddr',
-    packages = ['twAddr'],
+    packages = ['twaddr'],
     #scripts = ['runner'],
-    version = '1.0.3',
+    version = '1.0.6',
     description = 'Taiwan address related services',
     author = 'Ailan',
     author_email = 'karta2599434@gmail.com',
@@ -13,5 +25,5 @@ setup(
     url = 'https://github.com/ailan12345/twAddr',
 
     package_data = {'zipcode': ['*.csv']},
-
+    cmdclass = {'install': twAddr_install},
 )
