0abfc39c95f4e2ffcc9d0abe87ba449102dfd29d
ailan12345 <karta2599434@gmail.com>
2019-09-03 20:45:37 +0800

    populate data

--- twAddr/__init__.py
+++ twAddr/__init__.py
@@ -1,12 +1,17 @@
 import os
 
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.join(os.getcwd(), '桌面/twAddr/twAddr')
-print('開發 :', BASE_DIR)
+DEBUG = True
+DEBUG_PRINT = False
+
+if DEBUG:
+    BASE_DIR = os.path.join(os.getcwd(), '桌面/twAddr/twAddr')
+    print('開發 :', BASE_DIR)
+else:
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 countyCSV = os.path.join(BASE_DIR, 'county.csv')
-roadStreetCSV = os.path.join(BASE_DIR, 'roadStreet.db')
-villageCSV = os.path.join(BASE_DIR, 'village.db')
+roadStreetCSV = os.path.join(BASE_DIR, 'roadStreet.csv')
+villageCSV = os.path.join(BASE_DIR, 'village.csv')
 
 
 # # make a directory
--- twAddr/data.py
+++ twAddr/data.py
@@ -1,24 +1,78 @@
-from . import countyCSV, roadStreetCSV, villageCSV
+from . import countyCSV, roadStreetCSV, villageCSV, DEBUG_PRINT
 import csv
 import sqlite3
 
 conn = sqlite3.connect('test.db')
 print('Opened database successfully')
+
+# def existTable(tableName):
+#     c = conn.cursor()
+#     c.execute(''' SELECT count(name) FROM sqlite_master
+#                   WHERE type='table' AND name='{}' '''.format(tableName))
+#
+#     if c.fetchone()[0] != 0:
+#         conn.execute('DROP TABLE {}'.format(tableName))
+#         print(f'{tableName} Table drop successfully')
+#
+# existTable('COUNTY')
+# existTable('ROADSTREET')
+# existTable('VILLAGE')
+# conn.commit()
+#
+# # COUNTY
 # conn.execute('''CREATE TABLE COUNTY
 #                 (ZIPCODE CHAR(10) NOT NULL,
 #                 COUNTY CHAR(20) NOT NULL,
-#                 ENADDR TEXT NOT NULL);''')
-# print('Table created successfully')
+#                 ENCOUNTY TEXT NOT NULL);''')
+# print('COUNTY Table created successfully')
+#
+# csvfile = open(countyCSV, 'r')
+# rows = csv.reader(csvfile)
+#
+# for row in rows:
+#     conn.execute('insert into COUNTY values (?, ?, ?);', (
+#                 row[0],
+#                 row[1],
+#                 row[2]))
+# conn.commit()
+#
+# # ROADSTREET
+# conn.execute('''CREATE TABLE ROADSTREET
+#                 (ROADSTREET CHAR(30) NOT NULL,
+#                 ENROADSTREET TEXT NOT NULL);''')
+# print('ROADSTREET Table created successfully')
+#
+# csvfile = open(roadStreetCSV, 'r')
+# rows = csv.reader(csvfile)
+#
+# for row in rows:
+#     conn.execute('insert into ROADSTREET values (?, ?);', (
+#                 row[0],
+#                 row[1]))
+# conn.commit()
+#
+# # VILLAGE
+# conn.execute('''CREATE TABLE VILLAGE
+#                 (VILLAGE CHAR(30) NOT NULL,
+#                 ENVILLAGE TEXT NOT NULL);''')
+# print('VILLAGE Table created successfully')
+#
+# csvfile = open(villageCSV, 'r')
+# rows = csv.reader(csvfile)
+#
+# for row in rows:
+#     conn.execute('insert into VILLAGE values (?, ?);', (
+#                 row[0],
+#                 row[1]))
+# conn.commit()
+if not DEBUG_PRINT:
+    for row in conn.execute("SELECT * FROM COUNTY LIMIT 10"):
+        print(row)
 
-# conn.close()
+    for row in conn.execute("SELECT * FROM ROADSTREET LIMIT 10"):
+        print(row)
 
-csvfile = open(countyCSV, 'r')
-rows = csv.reader(csvfile)
-for row in rows:
-    conn.execute('insert into COUNTY values (?, ?, ?);', (
-                row[0],
-                row[1],
-                row[2]))
-for row in con.execute("SELECT * FROM COUNTY"):
-    print(row)
+    for row in conn.execute("SELECT * FROM VILLAGE LIMIT 10"):
+        print(row)
+
 conn.close()
